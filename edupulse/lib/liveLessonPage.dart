80164ebe5a9bf43741023962bea952af3a312f0a
Rishabh416 <mittalr2008@gmail.com>
2025-08-12 20:58:42 +0400

    audio stream added

--- edupulse/lib/liveLessonPage.dart
+++ edupulse/lib/liveLessonPage.dart
@@ -1,7 +1,76 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
 
-class LiveLessonPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:audio_streamer/audio_streamer.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+
+class LiveLessonPage extends StatefulWidget {
   const LiveLessonPage({Key? key}) : super(key: key);
+
+  @override
+  State<LiveLessonPage> createState() => _LiveLessonPageState();
+}
+
+class _LiveLessonPageState extends State<LiveLessonPage> {
+
+  int? sampleRate;
+  bool isRecording = false;
+  List<double> audio = [];
+  List<double>? latestBuffer;
+  double? recordingTime;
+  StreamSubscription<List<double>>? audioSubscription;
+
+  /// Check if microphone permission is granted.
+  Future<bool> checkPermission() async => await Permission.microphone.isGranted;
+
+  /// Request the microphone permission.
+  Future<void> requestPermission() async =>
+      await Permission.microphone.request();
+
+  /// Call-back on audio sample.
+  void onAudio(List<double> buffer) async {
+    audio.addAll(buffer);
+
+    // Get the actual sampling rate, if not already known.
+    sampleRate ??= await AudioStreamer().actualSampleRate;
+    recordingTime = audio.length / sampleRate!;
+
+    setState(() => latestBuffer = buffer);
+  }
+
+  /// Call-back on error.
+  void handleError(Object error) {
+    setState(() => isRecording = false);
+    print(error);
+  }
+
+  /// Start audio sampling.
+  void start() async {
+    // Check permission to use the microphone.
+    //
+    // Remember to update the AndroidManifest file (Android) and the
+    // Info.plist and pod files (iOS).
+    if (!(await checkPermission())) {
+      await requestPermission();
+    }
+
+    // Set the sampling rate - works only on Android.
+    AudioStreamer().sampleRate = 22100;
+
+    // Start listening to the audio stream.
+    audioSubscription =
+        AudioStreamer().audioStream.listen(onAudio, onError: handleError);
+
+    setState(() => isRecording = true);
+  }
+
+  /// Stop audio sampling.
+  void stop() async {
+    audioSubscription?.cancel();
+    setState(() => isRecording = false);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,9 +78,25 @@ class LiveLessonPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Live Lesson'),
       ),
-      body: const Center(
-        child: Text('Live Lesson Page'),
-      ),
+      body: Container(
+        margin: EdgeInsets.all(25),
+                    child: Column(children: [
+                      Container(
+                        child: Text(isRecording ? "Mic: ON" : "Mic: OFF",
+                            style: TextStyle(fontSize: 25, color: Colors.blue)),
+                        margin: EdgeInsets.only(top: 20),
+                      ),
+                      Text(''),
+                      Text('Max amp: ${latestBuffer?.reduce(max)}'),
+                      Text('Min amp: ${latestBuffer?.reduce(min)}'),
+                      Text(
+                          '${recordingTime?.toStringAsFixed(2)} seconds recorded.'),
+                    ])),
+                    floatingActionButton: FloatingActionButton(
+                      backgroundColor: isRecording ? Colors.red : Colors.green,
+                      child: isRecording ? Icon(Icons.stop) : Icon(Icons.mic),
+                      onPressed: isRecording ? stop : start,
+                    ),
     );
   }
 }
